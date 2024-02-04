ed8130f3d4f86338d94287d6b9ae039dc0d54b36
Gavin Castaneda <gcastaneda456@gmail.com>
2024-02-04 13:23:14 -0700

    Add required overrides to LytRacer

--- src/main/java/net/gooop/lytracer/LytRacer.java
+++ src/main/java/net/gooop/lytracer/LytRacer.java
@@ -3,5 +3,13 @@ package net.gooop.lytracer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LytRacer extends JavaPlugin {
+    // Called when plugin is first enabled
+    @Override
+    public void onEnable() {
+    }
 
+    // Called when plugin is disabled
+    @Override
+    public void onDisable() {
+    }
 }
