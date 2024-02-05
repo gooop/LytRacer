79df8a5458d1122a03f278b93aa65731ecff2693
Gavin Castaneda <gcastaneda456@gmail.com>
2024-02-04 22:34:03 -0700

    register lyt command

--- src/main/java/net/gooop/lytracer/LytRacer.java
+++ src/main/java/net/gooop/lytracer/LytRacer.java
@@ -1,11 +1,18 @@
+// Package name
 package net.gooop.lytracer;
 
+// Bukkit/Spigot/Paper Specific Imports
 import org.bukkit.plugin.java.JavaPlugin;
+
+// LytRacer Specific Imports
+import net.gooop.lytracer.commands.CommandLyt;
 
 public class LytRacer extends JavaPlugin {
     // Called when plugin is first enabled
     @Override
     public void onEnable() {
+        // Register our commands
+        this.getCommand("lyt").setExecutor(new CommandLyt());
     }
 
     // Called when plugin is disabled
