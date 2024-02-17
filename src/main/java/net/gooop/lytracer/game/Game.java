6efd09dc064cd3c5ab7f7027910f585c48e856a8
Gavin Castaneda <gcastaneda456@gmail.com>
2024-02-17 13:08:12 -0700

    Add document comments

--- src/main/java/net/gooop/lytracer/game/Game.java
+++ src/main/java/net/gooop/lytracer/game/Game.java
@@ -18,14 +18,25 @@ public class Game {
     private Timer timer;
     private Course course;
 
+    /**
+     * Constructor for Game class that includes id and course
+     * @param id The game ID assigned to this game instance.
+     * @param course The course associated with this game instance.
+     */
     public Game(UUID id, Course course) {
         this.course = course;
     }
 
+    /**
+     * Starts the game
+     */
     public void start() {
         timer = new Timer(course.getNumCheckpoints());
     }
     
+    /**
+     * Stops the game
+     */
     public void stop() {
         timer.stopTimer();
     }
