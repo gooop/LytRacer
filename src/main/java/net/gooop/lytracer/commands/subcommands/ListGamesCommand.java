6353f90be72ad5140be3635e614b31d00de30b09
Gavin Castaneda <gcastaneda456@gmail.com>
2024-06-07 22:01:22 -0600

    Create ListGamesCommand for debugging

---
+++ src/main/java/net/gooop/lytracer/commands/subcommands/ListGamesCommand.java
@@ -0,0 +1,60 @@
+/*
+ * Name: getGamesCommand
+ * Description: Command to start a game
+ * Author(s): Gooop
+ * License: MIT
+ */
+package net.gooop.lytracer.commands.subcommands;
+
+// LytRacer Specific Imports
+import net.gooop.lytracer.commands.LytCommand;
+import net.gooop.lytracer.game.Game;
+import net.gooop.lytracer.LytRacer;
+import net.gooop.lytracer.commands.CommandInfo;
+
+// Misc imports
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.Map.Entry;
+
+// Bukkit/Spigot/Paper Specific Imports
+import org.bukkit.command.CommandSender;
+
+@CommandInfo(name = "listgames")
+public class ListGamesCommand extends LytCommand {
+    private final String description = "Lists active games.";
+
+    @Override
+    public void run(LytRacer plugin, CommandSender sender, String[] args) {
+        sender.sendMessage("§3================== Active Games ==================§r");
+        for (Entry<UUID, Game> game : plugin.getGames().entrySet()) {
+            sender.sendMessage("Game: " + game.getKey().toString());
+        }
+        sender.sendMessage("§3================== Active Games ==================§r");
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String usage() {
+        //TODO: implement
+        return "";
+    }
+
+    @Override
+    public void help(LytRacer plugin, CommandSender sender, String[] args) {
+        //TODO: implement
+    }
+
+    @Override
+    public ArrayList<String> tabCompleteSuggestions(String[] args) {
+        // TODO Auto-generated method stub
+        ArrayList<String> suggestions = new ArrayList<>();
+        suggestions.add("startsuggest");
+        return suggestions;
+    }
+    
+}
